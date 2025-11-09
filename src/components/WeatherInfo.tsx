a7f4a6f7ec4c00cdae6121cd25f7def4eb4e2301
Kenarson <enggano299@gmail.com>
2025-11-09 11:57:31 +0700

    added api,data, and about

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './WeatherInfo.css';
 
 interface WeatherData {
-  city: string;
-  temperature: number;
-  description: string;
-  humidity: number;
-  windSpeed: number;
-  icon: string;
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  };
+  weather: Array<{
+    description: string;
+    icon: string;
+  }>;
+  wind: {
+    speed: number;
+  };
 }
 
 interface WeatherInfoProps {
@@ -17,24 +23,24 @@ interface WeatherInfoProps {
 const WeatherInfo: React.FC<WeatherInfoProps> = ({ weatherData }) => {
   return (
     <div className="weather-info">
-      <h2>{weatherData.city}</h2>
+      <h2>{weatherData.name}</h2>
       <div className="weather-main">
         <img 
-          src={`http://openweathermap.org/img/wn/${weatherData.icon}@2x.png`} 
-          alt={weatherData.description}
+          src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} 
+          alt={weatherData.weather[0].description}
           className="weather-icon"
         />
-        <div className="temperature">{Math.round(weatherData.temperature)}°C</div>
+        <div className="temperature">{Math.round(weatherData.main.temp)}°C</div>
       </div>
-      <div className="weather-description">{weatherData.description}</div>
+      <div className="weather-description">{weatherData.weather[0].description}</div>
       <div className="weather-details">
         <div className="detail">
           <span className="label">Humidity:</span>
-          <span className="value">{weatherData.humidity}%</span>
+          <span className="value">{weatherData.main.humidity}%</span>
         </div>
         <div className="detail">
           <span className="label">Wind Speed:</span>
-          <span className="value">{weatherData.windSpeed} m/s</span>
+          <span className="value">{weatherData.wind.speed} m/s</span>
         </div>
       </div>
     </div>
